e219353c4f0f4c60171a15d89f49b8b8f9de015c
johnd-rcl <98056458+johnd-rcl@users.noreply.github.com>
2023-10-17 14:07:17 -0400

    Update pax_opex_generator.py
    Updated styling in output window for more accessible text and added GUI functionality to permit generation of MD5 and SHA-1 checksums

--- pax_opex_generator.py
+++ pax_opex_generator.py
@@ -1,8 +1,8 @@
 import PySimpleGUI as sg
-from preservica_pre_ingest import *
-from preservica_post_ingest import *
-from documentation import *
-from utilities import *
+from preservica_pre_ingest import create_container, file_hash_list, folder_ds_files, representation_preservation_access, create_pax, pax_metadata, ao_opex_metadata, opex_container_metadata
+from preservica_post_ingest import test_connection, move_opex_aspace, move_aspace_trash, quality_control
+from documentation import pre_ingest_documentation, post_ingest_documentation, utilities_documentation, about
+from utilities import folder_desc_size
 from icon import icon
 
 ur_theme = {'BACKGROUND': '#003B71',
@@ -15,6 +15,11 @@ ur_theme = {'BACKGROUND': '#003B71',
             'BORDER': 1, 'SLIDER_DEPTH': 0, 'PROGRESS_DEPTH': 0
             }
 
+alt_background = '#181818'
+init_color = '#5dd495'
+update_color = '#7edcf0'
+summary_color = '#dcdca1'
+conclusion_color = '#e4a177'
 button_font = 'Courier 14'
 program_icon = icon()
 sg.set_options(font='Courier 12')
@@ -24,12 +29,12 @@ sg.user_settings_filename(path='.')
 
 preing_projfol_frame = sg.Frame('Project Folder', [
     [sg.Input(focus=True, expand_x=True), sg.FolderBrowse(key='-PROJFOLDER-')],
-    [sg.Push(), sg.Text('Generate File/Hash Manifest?'), sg.Checkbox('', default=True, key='-MANIFEST-'), sg.Push()]], expand_x=True, pad=5)
+    [sg.Push(), sg.Text('Generate File/Hash Manifest?'), sg.Checkbox('', default=True, key='-MANIFEST-'), sg.Combo(['', 'MD5', 'SHA-1'], default_value=sg.user_settings_get_entry('algorithm', default=''), key='-ALG-', readonly=True), sg.Push()]], expand_x=True, pad=5)
 
 preing_workord_frame = sg.Frame('Work Order Spreadsheet Information', [
                     [sg.Input(expand_x=True), sg.FileBrowse(file_types=(('Work Order', '*.xlsx'),), key='-WORKORDER-')],
                     [sg.Text('Worksheet Name', size=14, justification='right'), sg.Input(size=41, default_text='digitization_work_order_report', key='-WORKSHEET-')],
-                    [sg.Text('Max Row', size=14, justification='right'), sg.Input(size=5, key='-MAXROW-')],
+                    [sg.Text('Max Row', size=14, justification='right'), sg.Input(size=5, key='-MAXROW-')]
                     ], expand_x=True, pad=5)
 
 preing_fileext_frame = sg.Frame('Preservation/Access File Extensions', [
@@ -74,15 +79,15 @@ reports_frame = sg.Frame('Reports', [[sg.Text('Output Storage Size of Given Fold
 
 tab3 = sg.Tab('Utilities', [[reports_frame]], key='-TAB_3-')
 
-opscol = sg.TabGroup([[tab1, tab2, tab3]], pad=5, key='-TAB_GROUP-')
+ops_col = sg.TabGroup([[tab1, tab2, tab3]], pad=5, key='-TAB_GROUP-')
 
 settings_frame = sg.Frame('Settings',
                     [[sg.Button('Save', font=(button_font), pad=5), sg.Button('Display', font=(button_font), pad=5)]], pad=5)
 
-output_col = sg.Column([[sg.Output(key='-OUTPUT-', size = (60, 27), expand_x=True, pad=5)],
-                [settings_frame, sg.Button('Help', font=(button_font), pad=5), sg.Button('Clear', font=(button_font), pad=5), sg.Button('Exit', font=(button_font), pad=5), sg.Push(), sg.Button('About', font=(button_font), pad=5), sg.Sizegrip()]], expand_x=True, vertical_alignment='t')
+output_col = sg.Column([[sg.Output(key='-OUTPUT-', size = (60, 27), background_color='#252525', expand_x=True, pad=5)],
+                [settings_frame, sg.Button('Help', font=(button_font), pad=5), sg.Button('Clear', font=(button_font), pad=5), sg.Button('About', font=(button_font), pad=5), sg.Push(), sg.Button('Exit', font=(button_font), pad=5), sg.Sizegrip()]], expand_x=True, vertical_alignment='t')
 
-layout = [[opscol, output_col]]
+layout = [[ops_col, output_col]]
 
 window = sg.Window('OPEX/PAX Utility', layout, finalize=True, resizable=True, icon=program_icon)
 window.set_min_size(window.size)
@@ -92,6 +97,7 @@ while True:
     event, values = window.read()
     proj_path = values['-PROJFOLDER-']
     manifest = values['-MANIFEST-']
+    alg_choice = values['-ALG-']
     work_order = values['-WORKORDER-']
     work_sheet = values['-WORKSHEET-']
     max_row = values['-MAXROW-']
@@ -113,6 +119,8 @@ while True:
     if event in (sg.WIN_CLOSED, 'Exit'):
         break
     if event == 'Save':
+        sg.user_settings_set_entry('generate manifest', values['-MANIFEST-'])
+        sg.user_settings_set_entry('algorithm', values['-ALG-'])
         sg.user_settings_set_entry('format dates', values['-DATES-'])
         sg.user_settings_set_entry('file delimiter', values['-DELIMITER-'])
         sg.user_settings_set_entry('username', values['-USERNAME-'])
@@ -129,50 +137,50 @@ while True:
         count = 0
         for key in sg.user_settings():
             if (count % 2) == 0:
-                mline.update('{}: {}\n'.format(key, sg.user_settings()[key]), append=True, text_color_for_value='black', background_color_for_value='white')
+                mline.update('{}: {}\n'.format(key, sg.user_settings()[key]), append=True, text_color_for_value=update_color)
                 count += 1
             else:
-                mline.update('{}: {}\n'.format(key, sg.user_settings()[key]), append=True, text_color_for_value='white', background_color_for_value='black')
+                mline.update('{}: {}\n'.format(key, sg.user_settings()[key]), append=True, text_color_for_value=update_color, background_color_for_value=alt_background)
                 count += 1
     if event == 'Create PAX Objects and OPEX Metadata':
         mline.update('')
-        create_container(window, mline, proj_path, work_order)
+        create_container(window, mline, init_color, summary_color, proj_path, work_order)
         if manifest == True:
-            file_hash_list(window, mline, proj_path)
-        folder_ds_files(window, mline, proj_path, filename_delimiter)
-        representation_preservation_access(window, mline, proj_path, rep_pres, rep_acc)
-        create_pax(window, mline, proj_path)
-        pax_metadata(window, mline, proj_path, work_order, work_sheet, max_row, format_dates)
-        ao_opex_metadata(window, mline, proj_path, work_order, work_sheet, max_row)
-        opex_container_metadata(window, mline, proj_path)
-        mline.update('----PAX OBJECTS AND OPEX METADATA CREATED----\n', append=True, text_color_for_value='yellow', background_color_for_value='blue')
+            file_hash_list(window, mline, alt_background, init_color, update_color, summary_color, proj_path, alg_choice)
+        folder_ds_files(window, mline, alt_background, init_color, update_color, summary_color, proj_path, filename_delimiter)
+        representation_preservation_access(window, mline, alt_background, init_color, update_color, summary_color, proj_path, rep_pres, rep_acc)
+        create_pax(window, mline, alt_background, init_color, update_color, summary_color, proj_path)
+        pax_metadata(window, mline, alt_background, init_color, update_color, summary_color, proj_path, work_order, work_sheet, max_row, format_dates)
+        ao_opex_metadata(window, mline, alt_background, init_color, update_color, summary_color, proj_path, work_order, work_sheet, max_row)
+        opex_container_metadata(window, mline, init_color, update_color, proj_path)
+        mline.update('****PAX OBJECTS AND OPEX METADATA CREATED****\n', append=True, text_color_for_value=conclusion_color)
         window.refresh()
     if event == 'Test Connection':
-        test_connection(window, mline, username, password, tenancy, server, twofactorcb, twofactorkey)
+        test_connection(window, mline, alt_background, init_color, update_color, summary_color, username, password, tenancy, server, twofactorcb, twofactorkey)
     if event == 'Move From OPEX to ASpace Link':
-        move_opex_aspace(window, mline, username, password, tenancy, server, twofactorcb, twofactorkey, opex_folder, aspace_folder)
+        move_opex_aspace(window, mline, alt_background, init_color, update_color, summary_color, username, password, tenancy, server, twofactorcb, twofactorkey, opex_folder, aspace_folder)
     if event == 'Move From ASpace Link to Trash':
-        move_aspace_trash(window, mline, username, password, tenancy, server, twofactorcb, twofactorkey, aspace_folder, trash_folder)
+        move_aspace_trash(window, mline, alt_background, init_color, update_color, summary_color, username, password, tenancy, server, twofactorcb, twofactorkey, aspace_folder, trash_folder)
     if event == 'Quality Control':
-        quality_control(window, mline, username, password, tenancy, server, twofactorcb, twofactorkey, qual_control, work_order, work_sheet, max_row)
+        quality_control(window, mline, alt_background, init_color, update_color, summary_color, username, password, tenancy, server, twofactorcb, twofactorkey, qual_control, work_order, work_sheet, max_row)
     if event == 'Generate Storage Report on Folder':
-        folder_desc_size(window, mline, username, password, tenancy, server, twofactorcb, twofactorkey, folder_size)
+        folder_desc_size(window, mline, alt_background, init_color, update_color, summary_color, username, password, tenancy, server, twofactorcb, twofactorkey, folder_size)
     if event == 'Help':
         if values['-TAB_GROUP-'] == '-TAB_1-':
             mline.update('')
-            pre_ingest_documentation()
+            pre_ingest_documentation(window, mline, init_color, update_color)
             mline.set_vscroll_position(0)
         if values['-TAB_GROUP-'] == '-TAB_2-':
             mline.update('')
-            post_ingest_documentation()
+            post_ingest_documentation(window, mline, init_color, update_color, summary_color)
             mline.set_vscroll_position(0)
         if values['-TAB_GROUP-'] == '-TAB_3-':
             mline.update('')
-            utilities_documentation()
+            utilities_documentation(window, mline, init_color, update_color)
             mline.set_vscroll_position(0)
     if event == 'Clear':
         mline.update('')
     if event == 'About':
         mline.update('')
-        about()
+        about(window, mline, update_color)
 window.close()
