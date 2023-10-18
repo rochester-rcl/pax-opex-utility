96b08605f8b7fab45a7dce14e99c5e819406d06c
johnd-rcl <98056458+johnd-rcl@users.noreply.github.com>
2023-10-18 11:44:00 -0400

    Update pax_opex_generator.py
    Updated the layout elements starting in line 30. Standardized tab indenting so that different layout elements have consistent tab idents for easier legibility:
    Frame: 2 tabs
    Tab: 1 tab (each tab has one or more frames)
    Column: 0 tab (each column has the major layout elements, a tab group or the output window/system buttons)

--- pax_opex_generator.py
+++ pax_opex_generator.py
@@ -28,64 +28,84 @@ sg.theme('UR Theme')
 sg.user_settings_filename(path='.')
 
 preing_projfol_frame = sg.Frame('Project Folder', [
-    [sg.Input(focus=True, expand_x=True), sg.FolderBrowse(key='-PROJFOLDER-')],
-    [sg.Push(), sg.Text('Generate File/Hash Manifest?'), sg.Checkbox('', default=True, key='-MANIFEST-'), sg.Combo(['', 'MD5', 'SHA-1'], default_value=sg.user_settings_get_entry('algorithm', default=''), key='-ALG-', readonly=True), sg.Push()]], expand_x=True, pad=5)
+        [sg.Input(focus=True, expand_x=True), sg.FolderBrowse(key='-PROJFOLDER-')],
+        [sg.Push(), sg.Text('Generate File/Hash Manifest?'), sg.Checkbox('', default=True, key='-MANIFEST-'), sg.Combo(['', 'MD5', 'SHA-1'], default_value=sg.user_settings_get_entry('algorithm', default=''), key='-ALG-', readonly=True), sg.Push()]
+        ], expand_x=True, pad=5)
 
 preing_workord_frame = sg.Frame('Work Order Spreadsheet Information', [
-                    [sg.Input(expand_x=True), sg.FileBrowse(file_types=(('Work Order', '*.xlsx'),), key='-WORKORDER-')],
-                    [sg.Text('Worksheet Name', size=14, justification='right'), sg.Input(size=41, default_text='digitization_work_order_report', key='-WORKSHEET-')],
-                    [sg.Text('Max Row', size=14, justification='right'), sg.Input(size=5, key='-MAXROW-')]
-                    ], expand_x=True, pad=5)
+        [sg.Input(expand_x=True), sg.FileBrowse(file_types=(('Work Order', '*.xlsx'),), key='-WORKORDER-')],
+        [sg.Text('Worksheet Name', size=14, justification='right'), sg.Input(size=41, default_text='digitization_work_order_report', key='-WORKSHEET-')],
+        [sg.Text('Max Row', size=14, justification='right'), sg.Input(size=5, key='-MAXROW-')]
+        ], expand_x=True, pad=5)
 
 preing_fileext_frame = sg.Frame('Preservation/Access File Extensions', [
-                    [sg.Text('Preservation', size=12, justification='right'), sg.Input(size=5, default_text ='.tif', key='-PRES-'), sg.Text('Access', size=12, justification='right'), sg.Input(size=5, default_text='.pdf', key='-ACC-'), sg.Push()]], expand_x=True, pad=5)
+        [sg.Text('Preservation', size=12, justification='right'), sg.Input(size=5, default_text ='.tif', key='-PRES-'), sg.Text('Access', size=12, justification='right'), sg.Input(size=5, default_text='.pdf', key='-ACC-'), sg.Push()]
+        ], expand_x=True, pad=5)
 
-preing_options_frame = sg.Frame('Options', [[sg.Text('Format Dates', size=18, justification='right'), sg.Checkbox('', default=sg.user_settings_get_entry('format dates', default=True), key='-DATES-')],
-                                            [sg.Text('Filename Delimiter'), sg.Input(size=1, default_text=sg.user_settings_get_entry('file delimiter', default='-'), key='-DELIMITER-')]], expand_x=True, pad=5)
+preing_options_frame = sg.Frame('Options', [
+        [sg.Text('Format Dates', size=18, justification='right'), sg.Checkbox('', default=sg.user_settings_get_entry('format dates', default=True), key='-DATES-')],
+        [sg.Text('Filename Delimiter'), sg.Input(size=1, default_text=sg.user_settings_get_entry('file delimiter', default='-'), key='-DELIMITER-')]
+        ], expand_x=True, pad=5)
 
-tab1 = sg.Tab('Pre-Ingest', [[preing_projfol_frame],
-                             [preing_workord_frame],
-                             [preing_fileext_frame],
-                             [preing_options_frame],
-                             [sg.Push(), sg.Button('Create PAX Objects and OPEX Metadata', pad=(10, 5)), sg.Push()]], key='-TAB_1-')
+tab1 = sg.Tab('Pre-Ingest', [
+    [preing_projfol_frame],
+    [preing_workord_frame],
+    [preing_fileext_frame],
+    [preing_options_frame],
+    [sg.Push(), sg.Button('Create PAX Objects and OPEX Metadata', pad=(10, 5)), sg.Push()]
+    ], key='-TAB_1-')
 
-posting_pres_frame = sg.Frame('Preservica Administrator Credentials',
-                    [[sg.Text('Username', size=10, justification='right'), sg.Input(default_text=sg.user_settings_get_entry('username', default=''), key='-USERNAME-')],
-                    [sg.Text('Password', size=10, justification='right'), sg.Input(default_text=sg.user_settings_get_entry('password', default=''), password_char='*', key='-PASSWORD-')],
-                    [sg.Text('Tenancy', size=10, justification='right'), sg.Input(default_text=sg.user_settings_get_entry('tenancy', default=''), key='-TENANCY-')],
-                    [sg.Text('Server', size=10, justification='right'), sg.Input(default_text=sg.user_settings_get_entry('server', default='us.preservica.com'), key='-SERVER-')],
-                    [sg.Text('Using 2FA?', size=10, justification='right'), sg.Checkbox('', default=sg.user_settings_get_entry('twofa_cb', default=False), key='-2FACB-')],
-                    [sg.Text('2FA Token', size=10, justification='right'), sg.Input(default_text=sg.user_settings_get_entry('twofa_value', default=''), password_char='*', key='-2FA-')],
-                    [sg.Push(), sg.Button('Test Connection', pad=(10,5)), sg.Push()]], pad=5, expand_x=True)
+posting_pres_frame = sg.Frame('Preservica Administrator Credentials', [
+        [sg.Text('Username', size=10, justification='right'), sg.Input(default_text=sg.user_settings_get_entry('username', default=''), key='-USERNAME-')],
+        [sg.Text('Password', size=10, justification='right'), sg.Input(default_text=sg.user_settings_get_entry('password', default=''), password_char='*', key='-PASSWORD-')],
+        [sg.Text('Tenancy', size=10, justification='right'), sg.Input(default_text=sg.user_settings_get_entry('tenancy', default=''), key='-TENANCY-')],
+        [sg.Text('Server', size=10, justification='right'), sg.Input(default_text=sg.user_settings_get_entry('server', default='us.preservica.com'), key='-SERVER-')],
+        [sg.Text('Using 2FA?', size=10, justification='right'), sg.Checkbox('', default=sg.user_settings_get_entry('twofa_cb', default=False), key='-2FACB-')],
+        [sg.Text('2FA Token', size=10, justification='right'), sg.Input(default_text=sg.user_settings_get_entry('twofa_value', default=''), password_char='*', key='-2FA-')],
+        [sg.Push(), sg.Button('Test Connection', pad=(10,5)), sg.Push()]
+        ], pad=5, expand_x=True)
 
-posting_refid_frame = sg.Frame('Preservica Folder Ref IDs',
-                    [[sg.Text('OPEX Folder Ref', size=19, justification='right'), sg.Input(size=(36,1), default_text=sg.user_settings_get_entry('opex', default=''), key='-OPEX-')],
-                    [sg.Text('ASpace Folder Ref', size=19, justification='right'), sg.Input(size=(36,1), default_text=sg.user_settings_get_entry('aspace', default=''), key='-ASPACE-')],
-                    [sg.Text('Trash Folder Ref', size=19, justification='right'), sg.Input(size=(36,1), default_text=sg.user_settings_get_entry('trash', default=''), key='-TRASH-')],
-                    [sg.Push(), sg.Button('Move From OPEX to ASpace Link', pad=(10, 5)), sg.Push()], 
-                    [sg.Push(), sg.Button('Move From ASpace Link to Trash', pad=(10, 5)), sg.Push()]], pad=5)
+posting_refid_frame = sg.Frame('Preservica Folder Ref IDs', [
+        [sg.Text('OPEX Folder Ref', size=19, justification='right'), sg.Input(size=(36,1), default_text=sg.user_settings_get_entry('opex', default=''), key='-OPEX-')],
+        [sg.Text('ASpace Folder Ref', size=19, justification='right'), sg.Input(size=(36,1), default_text=sg.user_settings_get_entry('aspace', default=''), key='-ASPACE-')],
+        [sg.Text('Trash Folder Ref', size=19, justification='right'), sg.Input(size=(36,1), default_text=sg.user_settings_get_entry('trash', default=''), key='-TRASH-')],
+        [sg.Push(), sg.Button('Move From OPEX to ASpace Link', pad=(10, 5)), sg.Push()], 
+        [sg.Push(), sg.Button('Move From ASpace Link to Trash', pad=(10, 5)), sg.Push()]
+        ], pad=5)
 
-posting_qc_frame = sg.Frame('Quality Control',
-                    [[sg.Input(size=48, expand_x=True), sg.FileBrowse(file_types=(('File/Hash Manifest', '*.csv'),), key='-QC-')],
-                    [sg.Push(), sg.Button('Quality Control', pad=(10, 5)), sg.Push()]], expand_x=True, pad=5)
+posting_qc_frame = sg.Frame('Quality Control', [
+        [sg.Input(size=48, expand_x=True), sg.FileBrowse(file_types=(('File/Hash Manifest', '*.csv'),), key='-QC-')],
+        [sg.Push(), sg.Button('Quality Control', pad=(10, 5)), sg.Push()]
+        ], expand_x=True, pad=5)
 
-tab2 = sg.Tab('Post-Ingest', [[posting_pres_frame],
-                            [posting_refid_frame],
-                            [posting_qc_frame]], key='-TAB_2-')
+tab2 = sg.Tab('Post-Ingest', [
+    [posting_pres_frame],
+    [posting_refid_frame],
+    [posting_qc_frame]
+    ], key='-TAB_2-')
 
-reports_frame = sg.Frame('Reports', [[sg.Text('Output Storage Size of Given Folder')],
-                                     [sg.Text('Folder Ref:'), sg.Input(size=(36,1), key='-FOLDER_SIZE-')],
-                                     [sg.Button('Generate Storage Report on Folder', pad=(10, 5))]], expand_x=True, pad=5)
+reports_frame = sg.Frame('Reports', [
+        [sg.Text('Output Storage Size of Given Folder')],
+        [sg.Text('Folder Ref:'), sg.Input(size=(36,1), key='-FOLDER_SIZE-')],
+        [sg.Button('Generate Storage Report on Folder', pad=(10, 5))]
+        ], expand_x=True, pad=5)
 
-tab3 = sg.Tab('Utilities', [[reports_frame]], key='-TAB_3-')
+tab3 = sg.Tab('Utilities', [
+    [reports_frame]
+    ], key='-TAB_3-')
 
-ops_col = sg.TabGroup([[tab1, tab2, tab3]], pad=5, key='-TAB_GROUP-')
+ops_col = sg.TabGroup([
+[tab1, tab2, tab3]
+], pad=5, key='-TAB_GROUP-')
 
-settings_frame = sg.Frame('Settings',
-                    [[sg.Button('Save', font=(button_font), pad=5), sg.Button('Display', font=(button_font), pad=5)]], pad=5)
+settings_frame = sg.Frame('Settings', [
+        [sg.Button('Save', font=(button_font), pad=5), sg.Button('Display', font=(button_font), pad=5)]
+        ], pad=5)
 
-output_col = sg.Column([[sg.Output(key='-OUTPUT-', size = (60, 27), background_color='#252525', expand_x=True, pad=5)],
-                [settings_frame, sg.Button('Help', font=(button_font), pad=5), sg.Button('Clear', font=(button_font), pad=5), sg.Button('About', font=(button_font), pad=5), sg.Push(), sg.Button('Exit', font=(button_font), pad=5), sg.Sizegrip()]], expand_x=True, vertical_alignment='t')
+output_col = sg.Column([
+[sg.Output(key='-OUTPUT-', size = (60, 27), background_color='#252525', expand_x=True, pad=5)],
+[settings_frame, sg.Button('Help', font=(button_font), pad=5), sg.Button('Clear', font=(button_font), pad=5), sg.Button('About', font=(button_font), pad=5), sg.Push(), sg.Button('Exit', font=(button_font), pad=5), sg.Sizegrip()]
+], expand_x=True, vertical_alignment='t')
 
 layout = [[ops_col, output_col]]
 
